312c65629f310dabe8507a266166e73a4faa2d7e
Andrew Kent <andrew.kent@datadoghq.com>
2018-09-16 15:40:23 -0700

    wip: very basic testing

--- dd-java-agent/instrumentation/autotracing/src/main/java/datadog/trace/instrumentation/autotrace/AutoTraceInstrumentation.java
+++ dd-java-agent/instrumentation/autotracing/src/main/java/datadog/trace/instrumentation/autotrace/AutoTraceInstrumentation.java
@@ -15,10 +15,12 @@ import io.opentracing.tag.Tags;
 import io.opentracing.util.GlobalTracer;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 import static io.opentracing.log.Fields.ERROR_OBJECT;
+import static net.bytebuddy.matcher.ElementMatchers.any;
 
 @Slf4j
 @AutoService(Instrumenter.class)
@@ -43,15 +45,24 @@ public final class AutoTraceInstrumentation extends Instrumenter.Default {
   public Map<ElementMatcher, String> transformers() {
     // FIXME:
     final Map<ElementMatcher, String> transformers = new HashMap<>();
-    Collections.singletonMap(new ElementMatcher<TypeDescription>() {
+    transformers.put(new ElementMatcher<MethodDescription>() {
+      @Override
+      public boolean matches(MethodDescription target) {
+        return !target.isConstructor();
+      }
+    }, AutoTraceAdvice.class.getName());
+    return transformers;
+    /*
+    return Collections.singletonMap(new ElementMatcher<TypeDescription>() {
       @Override
       public boolean matches(TypeDescription target) {
         // FIXME matching all methods in a discovered node
+        System.out.println("  -- Autotrace Matches method? " + target.getName());
         return true;
       }
     }
     , AutoTraceAdvice.class.getName());
-    return transformers;
+    */
   }
 
   public static class AutoTraceAdvice {
@@ -68,12 +79,15 @@ public final class AutoTraceInstrumentation extends Instrumenter.Default {
                                               @Advice.Enter final long startTS,
                                               @Advice.Thrown final Throwable throwable) {
       // TODO
+      // System.out.println("TRACE METHOD? " + typeAndMethodName);
       if (startTS > 0l) {
         final Tracer globalTracer = GlobalTracer.get(); // TODO: move to method variable scope
         // ensure the active span was not removed in the method body
+        // System.out.println(" ^^ maybe: " + globalTracer.activeSpan());
         if (globalTracer.activeSpan() != null) {
           // TODO: span builder api also collects nano time
           final long durationNano = System.nanoTime() - startTS;
+          // System.out.println(" ^^ maybe2: " + durationNano);
           if (durationNano >= TraceDiscoveryGraph.AUTOTRACE_THRESHOLD_NANO) {
             final Scope scope = GlobalTracer.get().buildSpan(typeAndMethodName)
               .withTag(Tags.COMPONENT.getKey(), "autotrace")
--- dd-java-agent/instrumentation/autotracing/src/test/groovy/AutoTraceInstrumentationTest.groovy
+++ dd-java-agent/instrumentation/autotracing/src/test/groovy/AutoTraceInstrumentationTest.groovy
@@ -65,6 +65,34 @@ class AutoTraceInstrumentationTest extends AgentTestRunner {
         }
       }
     }
+
+    when:
+    TEST_WRITER.clear()
+
+    boolean slowTestRun = false
+    long t1 = System.nanoTime()
+    runUnderTrace("someTrace") {
+      new Helper1().someMethod(0)
+    }
+    long duration = System.nanoTime() - t1
+    if (duration >= TraceDiscoveryGraph.AUTOTRACE_THRESHOLD_NANO) {
+      // Trace is abnormally slow. Skip assertion.
+      slowTestRun = true
+    }
+
+    then:
+    slowTestRun ? true : assertTraces(TEST_WRITER, 1) {
+      trace(0, 1) {
+        span(0) {
+          serviceName "unnamed-java-app"
+          operationName "someTrace"
+          errored false
+          tags {
+            defaultTags()
+          }
+        }
+      }
+    }
   }
 
   static class Helper1 {
